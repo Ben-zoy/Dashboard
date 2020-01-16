60d48a02ecceca8f984179aa142874c8836830be
BenoitD34 <be.dugovic@gmail.com>
2020-01-16 15:06:41 +0100

    Dashboard Beta V0.1.3

--- Meteo.js
+++ Meteo.js
@@ -1,24 +1,39 @@
 // Récuperer l'adresse ip du pc qui ouvre la page //
-let url1 = "https://api.ipify.org?format=json"
+// let url1 = "https://api.ipify.org?format=json"
 
-let request1 = async () => {
-    let response1 = await fetch(url1);
-    let ip = await response1.json();
-    console.log(ip);
+// let request1 = async () => {
+//     let resultat = await fetch(url1);
+//     let ip = await resultat.json();
+//     console.log(ip);
 
 
 // Récuperer la ville grâce à l'adresse IP 
-const url2 = "http://freegeoip.io/json/"+ ip
+let url2 = "https://www.prevision-meteo.ch/services/json/montpellier";
 
 let request2 = async () => {
-    let response2 = await fetch(url2);
-    let ville = await response2.json();
+    let resultat = await fetch(url2);
+    let ville = await resultat.json();
     console.log(ville);
+    
+    let lameteo = document.getElementsByClassName("localite");
+    console.log(lameteo);
+    lameteo[0].innerHTML = ville.city_info.name;
+    console.log(ville.city_info.name);
+
+    let latemp = document.getElementsByClassName("temperature");
+    latemp[0].innerHTML = ville.current_condition.tmp;
+    console.log(ville.current_condition.tmp);
+
+    let licone = document.getElementsByClassName("licone");
+    console.log(licone);
+    licone[0].innerHTML = "<img src="  +ville.current_condition.icon_big + ">";
+    console.log(ville.current_condition.icon_big);
+    
 }
 request2();
-}
+// }
 
-request1();
+// request1();
 // Récuperer les infos météo de la ville 
 // const url3 = "https://api.meteomatics.com"
 
